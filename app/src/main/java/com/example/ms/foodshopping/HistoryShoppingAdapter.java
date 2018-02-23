188e49b95d13f77fef6a840cd3a817b91e35dd9e
MS <martinsalling325@hotmail.com>
2018-02-23 20:29:37 +0100

    History list added to tab 3

--- app/src/main/java/com/example/ms/foodshopping/HistoryShoppingAdapter.java
+++ app/src/main/java/com/example/ms/foodshopping/HistoryShoppingAdapter.java
@@ -1,8 +1,64 @@
 package com.example.ms.foodshopping;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * Created by MS on 23-02-2018.
  */
 
-public class HistoryShoppingAdapter {
+public class HistoryShoppingAdapter extends ArrayAdapter<Shopping> {
+
+    public HistoryShoppingAdapter(Context context, List<Shopping> weekTotals)
+    {
+        super(context, R.layout.row_historyshopping_custom, weekTotals);
+    }
+
+    private static class ViewHolder
+    {
+        TextView textViewYear;
+        TextView textViewShopName;
+        TextView textViewAmount;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        HistoryShoppingAdapter.ViewHolder holder;
+
+        if (convertView == null)
+        {
+            LayoutInflater myInflater = LayoutInflater.from(getContext());
+            convertView = myInflater.inflate(R.layout.row_historyshopping_custom, parent, false);
+
+            holder = new HistoryShoppingAdapter.ViewHolder();
+
+            holder.textViewYear = convertView.findViewById(R.id.textViewHistoryShoppingCustomRowYear);
+            holder.textViewShopName = convertView.findViewById(R.id.textViewHistoryShoppingCustomRowShopName);
+            holder.textViewAmount = convertView.findViewById(R.id.textViewHistoryShoppingCustomRowAmount);
+
+            convertView.setTag(holder);
+        }
+        else
+        {
+            holder = (HistoryShoppingAdapter.ViewHolder) convertView.getTag();
+        }
+
+        Shopping shopping = getItem(position);
+
+        if (shopping != null)
+        {
+            holder.textViewYear.setText("År: " + shopping.getYear());
+            holder.textViewShopName.setText(shopping.getShopName());
+            holder.textViewAmount.setText("Beløb: " + shopping.getAmount() + ",-");
+        }
+
+        return convertView;
+    }
 }
--- app/src/main/java/com/example/ms/foodshopping/Tab3.java
+++ app/src/main/java/com/example/ms/foodshopping/Tab3.java
@@ -6,6 +6,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by MS on 18-02-2018.
@@ -22,6 +26,40 @@ public class Tab3 extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        ShowHistoryShopping();
+    }
 
+    private void ShowHistoryShopping(){
+        ListView listViewTab3 = getView().findViewById(R.id.listViewTab3HistoryShopping);
+
+        ArrayList<Shopping> historyShoppingList = MainActivity.dbHelper.getHistoryShopping();
+        if (!historyShoppingList.isEmpty())
+        {
+            listViewTab3.setAdapter(null);
+            ListAdapter adapter = new HistoryShoppingAdapter(getActivity(), historyShoppingList);
+
+            if (listViewTab3 != null) {
+
+                listViewTab3.setAdapter(adapter);
+            }
+
+            /*listViewTab1.setOnItemClickListener(new AdapterView.OnItemClickListener()
+            {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+                {
+                    Movie selectedMovie = (Movie) parent.getItemAtPosition(position);
+
+                    Intent intent = new Intent(FavoritesActivity.this, DetailActivity.class);
+                    intent.putExtra("movieId", selectedMovie.Id);
+                    startActivity(intent);
+                }
+            });*/
+        }
+
+        else
+        {
+            listViewTab3.setAdapter(null);
+        }
     }
 }
